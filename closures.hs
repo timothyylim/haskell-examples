cc6c78393ae1c2568d4a081cc67042f70b91548d
timothyylim <timothylim23@gmail.com>
2019-10-03 12:39:50 +0200

    Add notes on closures and partial application

---
+++ closures.hs
@@ -0,0 +1,22 @@
+-- You can use partial application to 'start' functions and supply the parameters later
+-- Here is a function to generate a URL given some strings as parameters
+getRequestURL host apiKey resource id = host ++ 
+                                        "/" ++ 
+                                        resource ++
+                                        "/" ++
+                                        id ++
+                                        "?token=" ++
+                                        apiKey
+
+-- We can pre-supply the host if we know we are using the same host throughout our program:
+urlBuilderWithHost = getRequestURL "http://example.com"
+
+-- Call this function to test it
+print_UrlBuilderWithHost apiKey resource id = do
+  putStrLn $ urlBuilderWithHost apiKey resource id                                          
+
+-- We can even pre-supply all parameters except the id:
+urlBuilderWithHostAndResource = urlBuilderWithHost "1337hAsk3ll"
+
+print_UrlBuilderID id = do
+  putStrLn $ urlBuilderWithHost "API_KEY" "books"  id                                          
