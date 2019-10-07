abe744f6558fe061ba3855e1c72b0269ce1106f4
timothyylim <timothylim23@gmail.com>
2019-10-07 12:59:50 +0200

    Finish lists

--- lists.hs
+++ lists.hs
@@ -9,7 +9,19 @@ createLists = do
 
 -- Access element by index (!!)
 accessByIndex = print ([1,2,3] !! 0) 
-
 accessByIndex2 = (!!) "dog" -- use partial application for readability
 
+-- Check if a list contains an element 
+checkElExists = elem 13 [0, 13 .. 100]
+
+-- Take and drop
+myTake = take 5 [1 .. 10]
+myDrop = drop 5 [1 .. 10]
+
+-- Zip to combine lists into tuple pairs
+myZip = zip [1,2,3] [2,4,6]
+
+-- Cycle 
+ones n = take n (cycle [1])
+printOnes = ones
 
